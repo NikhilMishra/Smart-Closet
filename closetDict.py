99c66e85f9adf1fb188af5daa37a4adba18a50a2
NikhilMishra <mishranikh@gmail.com>
2019-11-09 13:26:54 -0600

    smart closet

---
+++ closetDict.py
@@ -0,0 +1,123 @@
+import json
+import webcolors
+import text2speech
+from playsound import playsound
+import math
+# createDict method
+
+# empty dictionary of closet items
+closetItems = {
+    "ffffff, pant": ["000000, shirt"],
+    "000000, pant": ["ffffff, shirt"],
+    "663300, pant": ["191970, shirt"],
+    "006400, pant": ["a9a9a9, shirt"],
+    "ff0000, pant": ["ffff00, shirt"],
+    "006600, pant": ["000000, shirt"],
+    "ff69b4, pant": ["708090, shirt"],
+    "ffffff, shirt": ["000000, pant"],
+    "000000, shirt": ["ffffff, pant"],
+    "663300, shirt": ["191970, pant"],
+    "006400, shirt": ["a9a9a9, pant"],
+    "ff0000, shirt": ["ffff00, pant"],
+    "006600, shirt": ["000000, pant"],
+    "ff69b4, shirt": ["708090, pant"]
+}
+
+# dictionary for colors
+color = {
+    # white to blue, red, black
+    "FFFFFF": ["0000FF", "FF0000", "000000"],
+    # red to yellow, white,
+    "FF0000": ["FFFF00", "FFFFFF"],
+    # brown to cream, green
+    "663300": ["FFFFCC", "006600"],
+    # orange to blue, white, black
+    "FFA500": ["0000FF", "FFFFFF", "000000"],
+    # green to black, brown
+    "006600": ["000000", "663300"]
+}
+
+# dictionary for items
+items = {
+    "pant": "shirt",
+    "shirt": "pant"
+}
+
+"""adds to dictionary as items are added.
+    key = color + value"""
+
+def getClosestColor(input):
+    value = input.split(', ')
+    color = tuple(int(value[0][i:i+2], 16) for i in (0, 2, 4))
+    # color = webcolors.hex_to_rgb(value[0])
+    item = value[1]
+    print("input" + item)
+    (r1,g1,b1) = color
+    min = 9999999999
+    returnVal = input
+    keys = closetItems.keys()
+    for i in range(len(closetItems)):
+        value1 = list(keys)
+        value2 = value1[i].split(', ')
+        color1 = value2[0]
+        item1 = value2[1]
+        (r2,g2,b2) = tuple(int(color1[i:i+2], 16) for i in (0, 2, 4))
+        if (math.sqrt((r1 - r2)**2 + (g1 - g2) ** 2 + (b1 - b2) **2)) < min and item1 == item:
+            print("in if")
+            print(item1 + ' ' + item)
+            min = math.sqrt((r1 - r2)**2 + (g1 - g2) ** 2 + (b1 - b2) **2)
+            returnVal = closetItems[value1[i]]
+
+    return returnVal
+
+def addDict(closetItems, key):
+    closetItems[key] = createsValue(key)
+
+
+def createsValue(key):
+    value = key.split(', ')
+    newColor = color.get(value[0])
+    newItem = items.get(value[1])
+    newValue = newColor[0] + ', ' + str(newItem)
+    return newValue
+
+#INPUT COLOR find closest value in closetItems dictionary
+
+def addArray(array, article):
+    #rgb = (int(float(array[0])), int(float(array[1])), int(float(array[2])))
+    rgb = tuple(array)
+    hex_result = '%02x%02x%02x' % rgb
+    print(hex_result)
+    input = hex_result + ', ' + article
+    item = getClosestColor(input)
+    recommendation = item
+    print(recommendation)
+    value = recommendation[0].split(', ')
+    color = value[0]
+    item = value[1]
+    print("Wear a " + webcolors.hex_to_name('#' + color) + " " + item)
+    subscription_key = "3dcb81c95f9d46248813f574677f3272"
+    app = text2speech.TextToSpeech(subscription_key, webcolors.hex_to_name('#' + color), article)
+    app.get_token()
+    app.save_audio("suggestion.wav")
+    playsound('suggestion.wav')
+
+
+
+def main():
+    print("Hello!")
+
+# userInputColor = input("What color (hex) is your item?")
+# # userInputColorHex = str('#{:02x}{:02x}{:02x}'.format(userInputColor))
+# rgb = (255, 255, 255)
+# hex_result = "".join([format(val, '02X') for val in rgb])
+# userInputItem = str(input("What item is it?"))
+# input = hex_result + ', ' + str(userInputItem)
+# addDict(closetItems, input)
+# print(closetItems)
+# recommendation = closetItems.get(input)
+# value = recommendation.split(', ')
+# color = value[0]
+# item = value[1]
+#
+# print("Wear a " + webcolors.hex_to_name('#' + color) + " " + item)
---
+++ smartCloset2.py
@@ -0,0 +1,103 @@
+import os
+import webcolors
+import text2speech
+import closetDict
+import camTest
+import speech2text
+import re
+from playsound import playsound
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd  + bd)] = name
+    return min_colours[min(min_colours.keys())]
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\nikhi\Downloads\smartCloset-522bf9518279.json"
+print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+from google.cloud import vision
+client = vision.ImageAnnotatorClient()
+import io
+from PIL import Image
+camTest.capture()
+# Opens a image in RGB mode
+im = Image.open('opencv.png')
+path = 'opencv.png'
+#suggest = input("what article of clothing do you want a suggestion for? ")
+subscription_key = "3dcb81c95f9d46248813f574677f3272"
+app = text2speech.TextToSpeech(subscription_key, "none", "none")
+app.get_token()
+app.save_audio("suggest.wav")
+playsound('suggest.wav')
+suggest = str(speech2text.userInput())
+suggest = re.findall(r'"([^"]*)"', suggest)[0].rstrip('.')
+print(suggest)
+with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+image = vision.types.Image(content=content)
+objects = client.object_localization(
+        image=image).localized_object_annotations
+for object_ in objects:
+    coord = [];
+    i = 0;
+    for vertex in object_.bounding_poly.normalized_vertices:
+        if i == 0:
+            coord.append(vertex.x)
+            coord.append(vertex.y)
+        if i == 1:
+            coord.append(vertex.x)
+        if i == 2:
+            coord.append(vertex.y)
+        i+=1
+    if (object_.name == "Top" or object_.name == "Shirt"):
+        width, height = im.size
+        # Cropped image of above dimension
+        # (It will not change orginal image)
+        im1 = im.crop((coord[0]*width, coord[1]*height, coord[2]*width, coord[3]*height))
+        im1.save("top.png")
+        path = "top.png"
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = client.image_properties(image=image)
+        props = response.image_properties_annotation
+        topColor = []
+        for color in props.dominant_colors.colors:
+            topColor.append(int(float(format(color.color.red))))
+            topColor.append(int(float(format(color.color.green))))
+            topColor.append(int(float(format(color.color.blue))))
+            break
+
+    if (object_.name == "Pants"):
+        width, height = im.size
+        # Cropped image of above dimension
+        # (It will not change orginal image)
+        im2 = im.crop((coord[0]*width, coord[1]*height, coord[2]*width, coord[3]*height))
+        im2.save("pant.png")
+
+        path = "pant.png"
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = client.image_properties(image=image)
+        props = response.image_properties_annotation
+        pantColor = []
+        for color in props.dominant_colors.colors:
+            pantColor.append(int(float(format(color.color.red))))
+            pantColor.append(int(float(format(color.color.green))))
+            pantColor.append(int(float(format(color.color.blue))))
+            break
+if suggest == "Pant" or suggest == "Pants" or suggest == "Paint" or suggest == "Paints":
+    for color in topColor:
+        print(int(float(color)))
+    closetDict.addArray(topColor, "shirt")
+else:
+    for color in pantColor:
+        print(int(float(color)))
+    closetDict.addArray(pantColor, "pant")
